19458fd34b5092d6e7f584800019824f447d4344
centertong <centertong@gmail.com>
2021-05-26 13:04:27 +0000

    add tree search

--- learned_index/lipp.go
+++ learned_index/lipp.go
@@ -4,12 +4,13 @@ import (
 	"container/list"
 	"fmt"
 )
+
 const entry_size = 1000
 const beta = 2
 const alpha = 0.1
 
 func InitIndex() *Index {
-	n := Node{A:1.0, b:0.0}
+	n := Node{A: 1.0, b: 0.0}
 	ind := &Index{n}
 	return ind
 }
@@ -29,59 +30,55 @@ func (ind *Index) Lookup(k uint32) (*Entry, bool) {
 		ek := e.d_.(DataType).k_
 		if ek == k {
 			return e, true
-		}		
+		}
 	}
 	return e, false
 }
 
-func (ind *Index) lookupPath(k uint32) (*Entry, bool, *list.List) {
+func (ind *Index) Insert(k uint32, p T) bool {
 	path := list.New()
-	node_ := &ind.root
-	path.PushBack(node_)
-	e := node_.forward(k)
+	n := &ind.root
+	path.PushBack(n)
+	e := n.forward(k)
 	for e.getType() == 3 { // 0: Null, 2: Data, 3:Node
-		node_ = e.d_.(NodeType).p_
-		e = node_.forward(k)
-		path.PushBack(node_)
+		n = e.d_.(NodeType).p_
+		e = n.forward(k)
+		path.PushBack(n)
 	}
+	chk := false
 	if e.getType() == 2 {
 		ek := e.d_.(DataType).k_
 		if ek == k {
-			return e, true, path
-		}		
-	}
-	return e, false, path
-}
+			fmt.Println("Same key exists")
+			return false
+		}
+		ep := e.d_.(DataType).v_
 
-
-func (ind *Index) Insert(k uint32, p T) bool {
-	e, chk, path := ind.lookupPath(k)
-	if chk {
-		fmt.Println("Same key exists")
-		return false
-	}
-
-	if e.getType() == 0 {
-		e.setType(2)
-		d := DataType{k, p}
-		e.d_ = d
-	} else {
-		ek := e.d_.(DataType).k_
-		
-		A_, b_:= trainNode([]uint32{k, ek}, entry_size)
-		n := &Node{A:A_, b:b_}
+		A_, b_ := trainNode([]uint32{k, ek}, entry_size)
 		e.setType(3)
+		n = &Node{A: A_, b: b_}
 		e.d_ = NodeType{n}
-	}	
+		
+		e = n.forward(k)
+		e.insert(k, p)
+		
+		e = n.forward(ek)
+		e.insert(ek, ep)
+		
+		n.element_num = 2
+		chk = true
+	} else {
+		e.insert(k, p)
+		
+	} 
 
-	chk = true
-	for p := path.Back() ; p != nil ; p = p.Prev() {
+	for p := path.Back(); p != nil; p = p.Prev() {
 		if p.Prev() != nil {
 			e = p.Prev().Value.(*Node).forward(k)
 			ind.adjust(p.Value.(*Node), e, chk)
 		} else {
 			ind.adjust(p.Value.(*Node), nil, chk)
-		}		
+		}
 		chk = false
 	}
 
@@ -93,22 +90,30 @@ func (ind *Index) adjust(n *Node, pe *Entry, chk bool) {
 	if chk {
 		n.conflict_num += 1
 	}
-	
-	if n.element_num >= beta * n.build_num && 
-		float32(n.conflict_num) / float32(n.element_num - n.build_num) >= alpha {
-			
-		pe.d_ = buildPartialTree(nil)
+	fmt.Println(n.element_num)
+	fmt.Println(n.conflict_num)
+	if n.element_num >= beta*n.build_num &&
+		float32(n.conflict_num)/float32(n.element_num-n.build_num) >= alpha {
+		ks := make([]uint32, n.element_num, n.element_num)
+		i := 0
+		n.getKeys(ks, &i)
+		
+		if i != int(n.element_num) {
+			fmt.Println("Error")
+		}
+		pe.d_ = buildPartialTree(ks)
 	}
 }
 
+
 func buildPartialTree(ks []uint32) *Node {
 	L := entry_size
-	if L < 2 * len(ks) {
+	if L < 2*len(ks) {
 		L = 2 * len(ks)
 	}
 	A_, b_ := trainNode(ks, uint32(L))
-	n := &Node{A:A_, b:b_}
-	for i := 0 ; i < L ; i += 1 {
+	n := &Node{A: A_, b: b_}
+	for i := 0; i < L; i += 1 {
 		subKs := []uint32{}
 		if len(subKs) == 1 {
 			n.e[i].setType(2)
@@ -118,7 +123,7 @@ func buildPartialTree(ks []uint32) *Node {
 			n.e[i].d_ = NodeType{buildPartialTree(subKs)}
 		}
 	}
-	n.element_num = uint32(len(ks))
+	n.build_num = uint32(len(ks))
 	n.element_num = uint32(len(ks))
 	n.conflict_num = 0
 	return n
@@ -129,49 +134,68 @@ func trainNode(ks []uint32, L uint32) (float32, float32) {
 	i := 0
 	T := 1
 	N := len(ks)
-	Ut :=  float32(ks[N-1-T] - ks[T]) / float32(L - 2)
-	for i <= N - 1 - T {
-		for i + T < N && float32(ks[i+T] - ks[i]) >= Ut {
+	fmt.Println(N - 1 - T)
+	fmt.Println(T)
+	fmt.Println(N)
+	Ut := float32(ks[N-1-T]-ks[T]) / float32(L-2)
+	for i <= N-1-T {
+		for i+T < N && float32(ks[i+T]-ks[i]) >= Ut {
 			i += 1
 		}
 
-		if i + T >= N {
+		if i+T >= N {
 			break
 		}
 
 		T += 1
-		Ut = float32(ks[N-1-T] - ks[T]) / float32(L - 2)
+		Ut = float32(ks[N-1-T]-ks[T]) / float32(L-2)
 	}
 
 	A_ := 1 / Ut
-	b_ := (entry_size - (A_ * float32(ks[N-1-T] + ks[T])))/ 2
+	b_ := (entry_size - (A_ * float32(ks[N-1-T]+ks[T]))) / 2
 	return A_, b_
 }
 
 type Node struct {
-	A float32
-	b float32
-	element_num uint32
+	A            float32
+	b            float32
+	element_num  uint32
 	conflict_num uint32
-	build_num uint32
-	e [entry_size]Entry
+	build_num    uint32
+	e            [entry_size]Entry
 }
+
+func (n *Node) getKeys(ks []uint32, idx *int) {
+	for i := 0; i < len(n.e); i++ {
+		if n.e[i].getType() == 3 {
+			n.e[i].d_.(NodeType).p_.getKeys(ks, idx)
+		} else if n.e[i].getType() == 2 {
+			ks[*idx] = n.e[i].d_.(DataType).k_
+			*idx += 1
+		}
+	}
+}
+
 
 func (n *Node) forward(k uint32) *Entry {
-	idx := int(n.A * float32(k) + n.b)
+	idx := int(n.A*float32(k) + n.b)
 
-	if len(n.e) - 1 <= idx {
-		return &n.e[len(n.e) - 1]
+	if len(n.e)-1 <= idx {
+		return &n.e[len(n.e)-1]
 	} else if idx <= 0 {
 		return &n.e[0]
-	}	
+	}
 	return &n.e[idx]
 }
-
 
 type Entry struct {
 	t_ uint8
 	d_ EntryType
+}
+
+func (e *Entry) insert(k uint32, p T) {
+	e.setType(2)
+	e.d_ = DataType{k, p}
 }
 
 func (e *Entry) getType() uint8 {
@@ -183,7 +207,6 @@ func (e *Entry) setType(t uint8) {
 }
 
 type EntryType interface {
-
 }
 
 type DataType struct {
@@ -191,13 +214,9 @@ type DataType struct {
 	v_ T
 }
 
-
 type NodeType struct {
-	p_ *Node 
+	p_ *Node
 }
-
-
-
 
 func main() {
 	fmt.Println("test")
@@ -209,9 +228,8 @@ func main() {
 	fmt.Println(chk)
 	ind.Insert(11, 3)
 	ind.Insert(12, 3)
-	
-}
 
+}
 
 type T interface {
 }
